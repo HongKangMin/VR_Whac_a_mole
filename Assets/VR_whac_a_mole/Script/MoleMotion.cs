238150173dbbbd739a197dbc07e32558fc483fb8
SURI <101981952+SURI913@users.noreply.github.com>
2023-05-26 12:07:25 +0900

    수정
    MoleMotion스크립트 수정

--- Assets/VR_whac_a_mole/Script/MoleMotion.cs
+++ Assets/VR_whac_a_mole/Script/MoleMotion.cs
@@ -7,12 +7,7 @@ public class MoleMotion : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 0;
     private Vector3 moveDirection = Vector3.zero;
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
